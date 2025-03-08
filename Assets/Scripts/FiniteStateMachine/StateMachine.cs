ad84c92423a1decff795fe6797a72f2187035b45
NhomToongTenz <truongduy18.03.03al@gmail.com>
2025-03-08 14:45:52 +0700

    adding resoucres

--- Assets/Scripts/FiniteStateMachine/StateMachine.cs
+++ Assets/Scripts/FiniteStateMachine/StateMachine.cs
@@ -1,9 +1,20 @@
+using System;
 using UnityEngine;
 
 namespace FiniteStateMachine
 {
-    public class StateMachine : MonoBehaviour
+    public abstract class StateMachine : MonoBehaviour
     {
-        
+        private State _currentState;
+
+        private void Update(){
+            _currentState?.Tick(Time.deltaTime);
+        }
+
+        public void SwitchState(State newState){
+            _currentState?.Exit();
+            _currentState = newState;
+            _currentState?.Enter();
+        }
     }
 }
