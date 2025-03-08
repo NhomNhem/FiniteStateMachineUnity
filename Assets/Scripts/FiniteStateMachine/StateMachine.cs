85b995d6d6ad0d972144fb2ff471024a38b7f7ca
NhemNhem <155875453+NhomNhem@users.noreply.github.com>
2025-03-08 20:28:36 +0700

    Revert "adding resoucres"
    This reverts commit ad84c92423a1decff795fe6797a72f2187035b45.

--- Assets/Scripts/FiniteStateMachine/StateMachine.cs
+++ Assets/Scripts/FiniteStateMachine/StateMachine.cs
@@ -1,20 +1,9 @@
-using System;
 using UnityEngine;
 
 namespace FiniteStateMachine
 {
-    public abstract class StateMachine : MonoBehaviour
+    public class StateMachine : MonoBehaviour
     {
-        private State _currentState;
-
-        private void Update(){
-            _currentState?.Tick(Time.deltaTime);
-        }
-
-        public void SwitchState(State newState){
-            _currentState?.Exit();
-            _currentState = newState;
-            _currentState?.Enter();
-        }
+        
     }
 }
