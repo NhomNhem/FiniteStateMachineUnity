09f56465504728f34f4379d392e0d5f2ed2f7552
DiverLight <tai874156@gmail.com>
2025-03-10 05:20:08 +0700

    Combat( 002 Targeting)

---
+++ Assets/MinhTai/Scripts/Combat/Targeting/Targeter.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Targeter : MonoBehaviour
+{
+
+   public List<Target> targets = new List<Target>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent<Target>(out Target target)) { return; }
+
+        targets.Add(target);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {    
+        if (!other.TryGetComponent<Target>(out Target target)) { return; }
+
+        targets.Remove(target);
+    }
+}
