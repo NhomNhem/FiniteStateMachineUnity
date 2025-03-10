1224d6b500bbf2f7a8ec2e5e42bff7b4babb69ac
DiverLight <tai874156@gmail.com>
2025-03-11 05:23:08 +0700

    007 Cinemachine Target

--- Assets/MinhTai/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/MinhTai/Scripts/Combat/Targeting/Targeter.cs
@@ -1,9 +1,11 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.Cinemachine;
 
 public class Targeter : MonoBehaviour
 {
+    [SerializeField] private CinemachineTargetGroup cinetargetGroup;
 
     private List<Target> targets = new List<Target>();
 
@@ -14,13 +16,14 @@ public class Targeter : MonoBehaviour
         if (!other.TryGetComponent<Target>(out Target target)) { return; }
 
         targets.Add(target);
+        target.OnDestroyed += RemoveTarget;
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (!other.TryGetComponent<Target>(out Target target)) { return; }
 
-        targets.Remove(target);
+        RemoveTarget(target);
     }
 
     public bool SelectTarget()
@@ -28,12 +31,28 @@ public class Targeter : MonoBehaviour
         if (targets.Count == 0) { return false; }
 
         CurrentTarget = targets[0];
+        cinetargetGroup.AddMember(CurrentTarget.transform, 1f, 2f);
 
         return true;
     }
 
     public void Cancel()
     {
+        if (CurrentTarget == null) { return; }
+
+        cinetargetGroup.RemoveMember(CurrentTarget.transform);
         CurrentTarget = null;
+    }
+
+    private void RemoveTarget(Target target)
+    {
+        if(CurrentTarget == target)
+        {
+            cinetargetGroup.RemoveMember(CurrentTarget.transform);
+            CurrentTarget = null;
+        }
+
+        target.OnDestroyed -= RemoveTarget;
+        targets.Remove(target);
     }
 }
