41d5bdd7592dba2d8ee658dd81fa76e1819a5b3c
DiverLight <tai874156@gmail.com>
2025-03-12 10:01:37 +0700

    015 Combo Attacks

--- Assets/MinhTai/Scripts/StateMachine/Player/Attack.cs
+++ Assets/MinhTai/Scripts/StateMachine/Player/Attack.cs
@@ -5,4 +5,10 @@ using System;
 public class Attack
 {
     [field: SerializeField] public string AnimationName { get; private set; }
+
+    [field: SerializeField] public float TransitionDuration { get; private set; }
+
+    [field: SerializeField] public int ComeboStateIndex { get; private set; } = -1;
+
+    [field: SerializeField] public float ComeboAttackTime { get; private set; }
 }
