09f64ff4b491557111a4686c1af2a6b071dde765
DiverLight <tai874156@gmail.com>
2025-03-14 03:32:51 +0700

    012 Ragdoll On Death

--- Assets/_MinhTai/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ Assets/_MinhTai/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -19,6 +19,8 @@ public class PlayerStateMachine : StateMachine
 
     [field: SerializeField] public Health Health { get; private set; }
 
+    [field: SerializeField] public Ragdoll Ragdoll { get; private set; }
+
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
 
     [field: SerializeField] public float TargetingMovementSpeed { get; private set; }
